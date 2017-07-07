93b805fff6c0e6051e700c3e5a2277db4579f736
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-06 20:08:02 -0700

    converted authenticate.js to async await

--- todo-api/server/middleware/authenticate.js
+++ todo-api/server/middleware/authenticate.js
@@ -1,17 +1,19 @@
 const { User } = require('../models/user')
 
-const authenticate = (req, res, next) => {
-  const token = req.header('x-auth')
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.header('x-auth')
+    const user = await User.findByToken(token)
 
-  User.findByToken(token).then((user) => {
     if (!user) {
       return Promise.reject({ name: 'HTTP Error 401', message: 'Unauthorized: Access is denied due to invalid credentials' })
     }
-
     req.user = user
     req.token = token
     next()
-  }).catch(e => res.status(401).send({ type: e.name, code: e.code, message: e.message }))
+  } catch (e) {
+    res.status(401).send({ type: e.name, code: e.code, message: e.message })
+  }
 }
 
 module.exports = { authenticate }
